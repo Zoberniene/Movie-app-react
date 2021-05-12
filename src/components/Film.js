0130323239d135b0eb3b40d84bf813c8855c41e1
Kristina Zobernienė <k.zigelyte@gmail.com>
2021-05-12 10:21:22 +0300

    Button onClick

--- src/components/Film.js
+++ src/components/Film.js
@@ -5,7 +5,9 @@ const Film = (props) => {
     <React.Fragment>
       {props.movies.map((movie, index) => (
         <div key={index}>
-          <img src={movie.Poster} alt="movie"></img>
+          <img src={movie.Poster} alt="movie cover"></img>
+          <h3>{movie.Title}</h3>
+          <p>{movie.Year}</p>
         </div>
       ))}
     </React.Fragment>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 const Search = (props) => {
+  const search = (e) => {
+    e.preventDefault();
+    /*props.setSearchValue("frozen");
+    console.log(e.target.value);*/
+    props.setSearchValue(e.target.value);
+    console.log(props.searchValue);
+  };
+
   return (
     <form>
       <input
         type="text"
         placeholder="Search for..."
         value={props.value}
-        onChange={(e) => props.setSearchValue(e.target.value)}
+        /*onChange={(e) => props.setSearchValue(e.target.value)}*/
       />
+      <button onClick={search}>Search</button>
     </form>
   );
 };
